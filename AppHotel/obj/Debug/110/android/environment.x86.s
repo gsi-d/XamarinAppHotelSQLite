13e44d94a5010d043290c8c95c4920ced2f90d97
Guilherme <guimota_silver@hotmail.com>
2022-01-17 17:29:59 -0300

    inciando migracao sqlite

--- AppHotel/obj/Debug/110/android/environment.x86.s
+++ AppHotel/obj/Debug/110/android/environment.x86.s
@@ -71,7 +71,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"834f9dd9-52ff-432e-8831-85b41d15e721"
+	.asciz	"2be618a7-3fca-416b-a2b7-3f119ca107af"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
